2279c192e4628b54f619f50a3d6672bffd816c53
student <student@DESKTOP-AD1HQ5K>
2022-10-14 17:20:21 +0900

    MyBootApplication conflict해결

--- myboot1-naver/src/main/java/com/example/demo/Myboot1Application.java
+++ myboot1-naver/src/main/java/com/example/demo/Myboot1Application.java
@@ -5,21 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-<<<<<<< HEAD
+
 //for ssl
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.apache.catalina.connector.Connector;
-=======
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.apache.catalina.connector.Connector;
 
->>>>>>> branch 'main' of https://github.com/sunghee-cho/naverserver202210.git
 /*  xml
  * <context:component-scan base-package=a.b.test">
  * @ComponentScan(basepackage="a.b.test")
@@ -37,10 +30,8 @@ import org.apache.catalina.connector.Connector;
 @MapperScan(basePackages ="board.spring.mybatis" )
 @MapperScan(basePackages = "upload")
 
-<<<<<<< HEAD
+
 //for ssl
-=======
->>>>>>> branch 'main' of https://github.com/sunghee-cho/naverserver202210.git
 @RestController
 public class Myboot1Application {
 
@@ -48,20 +39,6 @@ public class Myboot1Application {
 		SpringApplication.run(Myboot1Application.class, args);
 	}
 	
-	@Bean    
-	public ServletWebServerFactory serveltContainer(){       
-		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();        
-		tomcat.addAdditionalTomcatConnectors(createStandardConnector());       
-		return tomcat;    
-	}    
-	
-	private Connector createStandardConnector(){        
-		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");        
-		connector.setPort(8080);        
-		return connector;    
-	}
-
-
 	@Bean    
 	public ServletWebServerFactory serveltContainer(){       
 		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();        
