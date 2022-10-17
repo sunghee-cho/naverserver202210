a688f44d870d504366985a41666fe3efbcc8d63e
student <student@DESKTOP-AD1HQ5K>
2022-10-17 14:18:16 +0900

    105

--- myboot1-naver/src/main/java/com/example/demo/JasyptConfig.java
+++ myboot1-naver/src/main/java/com/example/demo/JasyptConfig.java
@@ -26,12 +26,12 @@ public class JasyptConfig {
 	
     @Bean("jasyptEncryptor")
     public StringEncryptor stringEncryptor() {
-    	//System.out.println(System.getenv("JASYPT_ENCRYPTOR_PASSWORD"));
-    	System.out.println(System.getenv("DB_PASSWORD"));
+    	//System.out.println(System.getenv("DB_PASSWORD"));
+    	System.out.println(System.getProperty("jasypt.encryptor.password"));
     	//System.out.println("전체환경변수값 : "+System.getenv());
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(System.getenv("DB_PASSWORD")); 
+        config.setPassword(System.getProperty("jasypt.encryptor.password")); 
         
         // 암호화 키. #jasypt.encryptor.password=root
         //config.setPassword(environment.getProperty("jasypt.encryptor.password"));
